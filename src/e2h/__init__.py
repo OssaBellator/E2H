2254a8607edf18d2c69a4a14c0bc1736c51c9309
OssaBellator <141500316+OssaBellator@users.noreply.github.com>
2026-08-05 22:17:08 +1000

    Export evidence ingestion APIs

--- src/e2h/__init__.py
+++ src/e2h/__init__.py
@@ -1,6 +1,7 @@
 """Evidence-to-Harness core package."""
 
 from e2h.experiment import ExperimentResult, ExperimentSpec, run_experiment
+from e2h.ingest import IngestionBundle, ingest_otlp_file, ingest_transcript_file
 from e2h.models import TaskCapsule
 from e2h.runner import RunResult, run_capsule
 from e2h.trace import Trace, TraceEvent, trace_from_run_result
@@ -8,12 +9,15 @@ from e2h.trace import Trace, TraceEvent, trace_from_run_result
 __all__ = [
     "ExperimentResult",
     "ExperimentSpec",
+    "IngestionBundle",
     "RunResult",
     "TaskCapsule",
     "Trace",
     "TraceEvent",
+    "ingest_otlp_file",
+    "ingest_transcript_file",
     "run_capsule",
     "run_experiment",
     "trace_from_run_result",
 ]
-__version__ = "0.2.0"
+__version__ = "0.3.0"
